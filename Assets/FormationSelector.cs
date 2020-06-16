df77fce72205c5263cc73fe553786acc77969ee1
Jazayeerly <ahmed.aljazayeerly@gmail.com>
2020-06-16 18:05:13 +0200

    PlayersLocation

---
+++ Assets/FormationSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationSelector : MonoBehaviour {
+
+	public GameObject[] formations;
+	// Use this for initialization
+	public void ActivateFormtion(int index)
+	{
+		for (int i = 0; i < formations.Length; i++)
+		{
+			if (i == index)
+			{
+				formations[i].SetActive(true);
+			}
+			else
+			{
+				formations[i].SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Script/Scriptable Objects/PlayerPositions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName = "Kandooz/SquadFomrtation")]
+public class PlayerPositions : ScriptableObject {
+
+	public List<Vector3> SqaudPositions;
+}
